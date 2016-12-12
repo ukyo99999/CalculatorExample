e0cdd20921492bff44af34b54c2a40c40aa2ee7b
ukyo <ukyo99999@gmail.com>
2016-12-13 00:35:17 +0800

    find views

--- app/src/main/java/net/ukyo/calculatorexample/MainActivity.java
+++ app/src/main/java/net/ukyo/calculatorexample/MainActivity.java
@@ -2,12 +2,55 @@ package net.ukyo.calculatorexample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    private TextView textResult;
+    private Button btn0;
+    private Button btn1;
+    private Button btn2;
+    private Button btn3;
+    private Button btn4;
+    private Button btn5;
+    private Button btn6;
+    private Button btn7;
+    private Button btn8;
+    private Button btn9;
+    private Button btnPuls;
+    private Button btnMinus;
+    private Button btnTimes;
+    private Button btnDivided;
+    private Button btnDel;
+    private Button btnDot;
+    private Button btnEquls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
+    }
+
+    private void findViews() {
+        btn0 = (Button) findViewById(R.id.btn_0);
+        btn1 = (Button) findViewById(R.id.btn_1);
+        btn2 = (Button) findViewById(R.id.btn_2);
+        btn3 = (Button) findViewById(R.id.btn_3);
+        btn4 = (Button) findViewById(R.id.btn_4);
+        btn5 = (Button) findViewById(R.id.btn_5);
+        btn6 = (Button) findViewById(R.id.btn_6);
+        btn7 = (Button) findViewById(R.id.btn_7);
+        btn8 = (Button) findViewById(R.id.btn_8);
+        btn9 = (Button) findViewById(R.id.btn_9);
+        btnPuls = (Button) findViewById(R.id.btn_plus);
+        btnMinus = (Button) findViewById(R.id.btn_minus);
+        btnTimes = (Button) findViewById(R.id.btn_times);
+        btnDivided = (Button) findViewById(R.id.btn_divided);
+        btnDel = (Button) findViewById(R.id.btn_del);
+        btnDot = (Button) findViewById(R.id.btn_dot);
+        btnEquls = (Button) findViewById(R.id.btn_equals);
     }
 }
